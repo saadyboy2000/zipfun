4158e159b89f5033126b7eb1c7b1afa38248a435
saaderana <saaderana@gmail.com>
2018-05-03 12:54:16 -0700

    newest version

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import React from "react";
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
-//import Name from "./components/Name";
+
 
 
 class App extends React.Component {
   state = {
     city: undefined,
-    //stores:[],//start here store[index].name
     store: undefined,
-    //store2: undefined,
-    //names: [],
-    //launch_date: undefined,store[index].launch_date
-    launch: undefined,
     error: undefined
   }
   getWeather = async (e) => {
@@ -22,23 +17,17 @@ class App extends React.Component {
     const zipcode = e.target.elements.zipcode.value;
     const api_call = await fetch(`https://shipt-zip-code-test-api.herokuapp.com/api/zip_codes/${zipcode}`);
     const data = await api_call.json();
-    //const names = data.stores.map(p=>p.name);
+
     if (zipcode) {
       this.setState({
         city: data.metro_name,
         store:data.stores[0].name,
-        //store2:data.stores[1].name,
-        launch: data.stores[0].launch_date,
-        //names: names,
         error: ""
       });
     } else {
       this.setState({
         city: undefined,
         store: undefined,
-        //store2: undefined,
-        launch: undefined,
-       //names: [],
         error: "Please enter the values."
       });
     }
@@ -58,10 +47,8 @@ class App extends React.Component {
                   <Weather 
                     city={this.state.city}
                     store ={this.state.store}
-                    launch = {this.state.launch_date}
                     error={this.state.error}
                   />
-                {/* <Name names= {this.state.names}/> */}
                 </div>
               </div>
             </div>
